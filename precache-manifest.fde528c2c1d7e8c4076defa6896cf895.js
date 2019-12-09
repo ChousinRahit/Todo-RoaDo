2eb37f1c0c3a47ebf471a03f9f4ad75978af7085
ChousinRahit <2428kcr@gmail.com>
2019-12-09 12:17:02 +0530

    Updates

---
+++ precache-manifest.fde528c2c1d7e8c4076defa6896cf895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70db45e5d1b1c945d424d4d9aa9c6ef1",
+    "url": "/Todo-RoaDo/index.html"
+  },
+  {
+    "revision": "f66fdc16799c9029e9d7",
+    "url": "/Todo-RoaDo/static/css/main.c3719f78.chunk.css"
+  },
+  {
+    "revision": "4290c73e400f0724ed57",
+    "url": "/Todo-RoaDo/static/js/2.bff2b661.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Todo-RoaDo/static/js/2.bff2b661.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f66fdc16799c9029e9d7",
+    "url": "/Todo-RoaDo/static/js/main.d500c04b.chunk.js"
+  },
+  {
+    "revision": "8b30a9c04dc576da1ab6",
+    "url": "/Todo-RoaDo/static/js/runtime-main.c61340f6.js"
+  }
+]);
